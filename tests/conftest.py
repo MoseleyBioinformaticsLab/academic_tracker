394f516e84b9bfffbd5e29d11493320feae3c1ff
Travis Thompson <ptth222@uky.edu>
2022-04-05 09:39:38 -0400

    Back to working CodeCov.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import academic_tracker
-print(academic_tracker.__file__)
+
 from academic_tracker import __main__
-print(__main__.__file__)
 
 @pytest.fixture(autouse=True)
 def set_verbose_and_silent(monkeypatch):

f08fecf12500417272f6473b0f5ea67ebe4a4281
Travis Thompson <ptth222@uky.edu>
2022-09-01 14:03:25 -0400

    Improved API documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
  
-from src.academic_tracker import __version__
+from academic_tracker import __version__
 
 
 # -- Project information -----------------------------------------------------
